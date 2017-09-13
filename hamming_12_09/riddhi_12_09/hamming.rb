a50463b6117836c9207b8aa1a38f4dac07eaa213
Riddhi <riddhi.mehta@bacancytechnology.com>
2017-09-13 09:37:57 +0530

    hamming exercise

--- hamming_12_09/riddhi_12_09/hamming.rb
+++ hamming_12_09/riddhi_12_09/hamming.rb
@@ -21,20 +21,19 @@
 # p hamming(string1,string2)
 
 
-
 class Hamming
 	def self.compute(dna1, dna2)
 		if dna1.length == dna2.length
-	    hamming_count = 0
-	    ch = dna2.split("")
-	    dna1.each_char.with_index(0) do |c, i|
-	      unless ch[i] == c.to_s
-	        hamming_count = hamming_count + 1
-	      end
-	    end
-	    hamming_count
+			hamming_count = 0
+			ch = dna2.split("")
+			dna1.each_char.with_index(0) do |c, i|
+				unless ch[i] == c.to_s
+					hamming_count = hamming_count + 1
+				end
+			end
+			hamming_count
 		else
-      raise ArgumentError
-    end
+			raise ArgumentError
+		end
 	end
 end
